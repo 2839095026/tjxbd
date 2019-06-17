581f9691af7da2e7e6502f6e64c7dc154907a19a
ovo-tj <1534198045@qq.com>
2019-06-17 14:28:03 +0800

    x

--- UI/UI/Controllers/salaryCriterionController.cs
+++ UI/UI/Controllers/salaryCriterionController.cs
@@ -20,7 +20,6 @@ namespace UI.Controllers
             return View();
         }
 
-        public void ss() { }
         // GET: salaryCriterion/Create
         public ActionResult Create()
         {
