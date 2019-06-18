be0b6c638067803b29174d2bc5b5feaa93c97615
ovo-tj <1534198045@qq.com>
2019-06-18 08:49:44 +0800

    新增了表，所以新增了四张表的七层

---
+++ UI/BLL/RolesBll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using IDAO;
+using IBLL;
+using IocContainer;
+namespace BLL
+{
+    //IBLL.IRolesBll bll = IocContainer.IocCreate.CreateBll<IBLL.IRolesBll>("RolesBll");
+
+    public class RolesBll : IRolesBll
+    {
+        IRolesDao<Roles> dao = IocCreate.CreateDao<IRolesDao<Roles>>("RolesDao");
+        public int Add(Roles t)
+        {
+            return dao.Add(t);
+        }
+
+        public int Change(Roles t)
+        {
+            return dao.Change(t);
+        }
+
+        public int Del(Roles t)
+        {
+            return dao.Del(t);
+        }
+
+        public List<Roles> FindAll()
+        {
+            return dao.FindAll();
+        }
+    }
+}
---
+++ UI/BLL/SalaryItemBll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using IBLL;
+using IocContainer;
+using IDAO;
+
+namespace BLL
+{
+    //IBLL.IBonusBll bll = IocContainer.IocCreate.CreateBll<IBLL.IBonusBll>("BonusBll");
+    public class SalaryItemBll : ISalaryItemBll
+    {
+        ISalaryItemDao<salary_item> dao = IocCreate.CreateDao<ISalaryItemDao<salary_item>>("SalaryItemDao");
+        public int Add(salary_item t)
+        {
+            return dao.Add(t);
+        }
+
+        public int Change(salary_item t)
+        {
+            return dao.Change(t);
+        }
+
+        public int Del(salary_item t)
+        {
+            return dao.Del(t);
+        }
+
+        public List<salary_item> FindAll()
+        {
+            return dao.FindAll();
+        }
+    }
+}
